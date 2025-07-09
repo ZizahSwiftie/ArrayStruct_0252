a22a07863f8165694a648a0212036651a64ca38f
ZizahSwiftie <azizahulnha@gmail.com>
2025-07-09 14:34:07 +0700

    Define Mahasiswa data structure

---
+++ struct_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Mendefinisikan struktur data untuk Mahasiswa
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    string alamat;
+};
+
+int main()
+{
+    // Fungsi main masih kosong, logika akan ditambahkan nanti
+}

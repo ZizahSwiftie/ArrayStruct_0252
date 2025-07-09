3e7c0704d0aeebd623335750d3efd3771dd00768
ZizahSwiftie <azizahulnha@gmail.com>
2025-07-09 14:26:11 +0700

    Implement data input for student array

--- netsed_struct.cpp
+++ netsed_struct.cpp
@@ -18,3 +18,37 @@ int main() {
     // Fungsi main masih kosong, akan diisi pada tahap berikutnya
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa {
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
+
+int main() {
+    // Membuat array dari object struct
+    Mahasiswa mhs[2];
+
+    // Menambahkan loop untuk mengisi data mahasiswa
+    for (int i = 0; i < 2; i++) {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukan nim = ";
+        cin >> mhs[i].nim;
+        cout << "Masukan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+    }
+
+    return 0;
+}
